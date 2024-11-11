374a96fc5bea3f96d88e14241179110d9253c1e7
Arash Sal Moslehian <arashmoslehian@protonmail.com>
2024-11-11 11:41:11 +0100

    Add cotracker submodule.

--- src/benchtrack/benchtrack_dj.py
+++ src/benchtrack/benchtrack_dj.py
@@ -1,9 +1,10 @@
 """
 Inspired from elements
 """
-import datajoint as dj
 import os
 import glob
+
+import datajoint as dj
 import cv2
 import pandas as pd
 import numpy as np
@@ -88,98 +89,4 @@ class VideoInfo(dj.Manual):
     num_frames      : int
     """
 
-
-def import_from_horse30(dir_path: str):
-    project_id = "proj1"
-    Project.insert1({
-        'project_id': project_id,
-        'project_name': 'benchtrack',
-        'project_description': f'Benchmarking tracking algorithms'
-    }, skip_duplicates=True)
-
-    for i, subject_name in enumerate(os.listdir(dir_path)):
-        subject_path = os.path.join(dir_path, subject_name)
-        if os.path.isdir(subject_path):
-            subject_id = f"sub-{i+1}"
-            session_id = f"ses-{i+1}"
-            
-            Subject.insert1({
-                'subject_id': subject_id,
-                'subject_name': subject_name,
-                'subject_description': f'Subject {subject_name}'
-            }, skip_duplicates=True)
-            
-            Session.insert1({
-                'project_id': project_id,
-                'session_id': session_id,
-                'session_description': f'Session for {subject_name}'
-            }, skip_duplicates=True)
-            
-            # Associate Subject with Session
-            Session.Subject.insert1({
-                'project_id': project_id,
-                'session_id': session_id,
-                'subject_id': subject_id
-            }, skip_duplicates=True)
-            
-            # Concatenate images into a matrix
-            image_files = sorted(glob.glob(os.path.join(subject_path, '*.png')))
-            frames = [cv2.imread(img) for img in image_files]
-            video = np.stack(frames, axis=0)
-            
-            video_id = f"vid-{i+1}_{subject_name}"
-            Video.insert1({
-                'project_id': project_id,
-                'session_id': session_id,
-                'video_id': video_id,
-                'video': video
-            }, skip_duplicates=True)
-            
-            VideoInfo.insert1({
-                'project_id': project_id,
-                'session_id': session_id,
-                'video_id': video_id,
-                'description': f'Video for {subject_name}',
-                'fps': 30.0,
-                'height': frames[0].shape[0],
-                'width': frames[0].shape[1],
-                'num_frames': len(frames)
-            }, skip_duplicates=True)
-            
-            # Parse H5 for body parts and annotations
-            hdf_path = os.path.join(subject_path, 'CollectedData_Byron.h5')
-            df = pd.read_hdf(hdf_path)
-            df = df.get(df.columns.levels[0][0]) # remove scorer name
-            body_parts = sorted(df.columns.levels[0].to_list())
-
-            Session.Segments.insert1({
-                'project_id': project_id,
-                'session_id': session_id,
-                'subject_id': subject_id,
-                'segment_names': body_parts
-            }, skip_duplicates=True)
-
-            # Prepare data for AnnotatedKeypoints
-            num_frames = len(df)
-            assert num_frames == len(video)
-            frame_indices = range(num_frames)
-            num_bodyparts = len(body_parts)
-            pose = np.zeros((num_frames, num_bodyparts, 2))
-            for frame_idx, (index, row) in enumerate(df.iterrows()):
-                for part_idx, part in enumerate(body_parts):
-                    pose[frame_idx, part_idx, 0] = row[part, 'x']
-                    pose[frame_idx, part_idx, 1] = row[part, 'y']
-            
-            Session.AnnotatedKeypoints.insert1({
-                'project_id': project_id,
-                'session_id': session_id,
-                'subject_id': subject_id,
-                'frame_idx': frame_indices,
-                'pose': pose,
-                'likelihood': None
-            }, skip_duplicates=True)
-            print(f"Imported {subject_name}.")
-
-
-if __name__ == "__main__":
-    import_from_horse30("/home/arash/dev/benchtrack/data/horse10/labeled-data")
+class TrackingBenchmark(dj.Computed):
